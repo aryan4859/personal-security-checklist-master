252fc4795a90227f4073435c8a5f7a6960430aa5
aryanstha <aryanstha4859@gmail.com>
2024-05-21 07:37:48 +0545

    Update about-content.ts

---
+++ web/src/routes/about/about-content.ts
@@ -0,0 +1,47 @@
+export const intro = [
+  `The objective of this project is to give you practical guidance on how to improve your digital security, and protect your privacy online.`,
+  `
+  The checklist is a living document, and will be updated regularly to reflect the latest threats and best practices.
+  This is made possible by open sourcing the content, and making it a community maintained resource,
+  meaning that anyone can suggest changes, make additions or update the guidance.
+  All edits are then reviewed by maintainers before being merged and going live.
+  `];
+
+export const contributing = [
+   
+];
+
+export const projects = [
+   
+];
+
+export const socials = [
+  {
+    title: 'GitHub',
+    icon: 'hub',
+    link: '',
+  },
+  {
+    title: 'Twitter',
+    icon: 'twitter',
+    link: ' ',
+  },
+  {
+    title: 'Mastodon',
+    icon: 'mastodon',
+    link: '',
+  },
+  {
+    title: 'Dev',
+    icon: 'dev',
+    link: '',
+  },
+  {
+    title: 'LinkedIn',
+    icon: 'linkedin',
+    link: '',
+  },
+];
+
+export const license = `
+The MIT License (MIT) `;
